37e9a6cfa35363ad407736b32f9c322aebb80766
Istiak26615 <istiak.shishir26615@gmail.com>
2022-01-05 21:59:53 +0600

    heroku link added

--- src/Pages/Dashboard/Dashboard/MakeAdmin.js
+++ src/Pages/Dashboard/Dashboard/MakeAdmin.js
@@ -12,7 +12,7 @@ export default function MakeAdmin() {
     const handleAdminSubmit = e => {
         const user = { email };
 
-        fetch('http://localhost:5000/users/admin', {
+        fetch('https://cryptic-plains-35102.herokuapp.com/users/admin', {
             method: 'PUT',
             headers: {
                 'content-type': 'application/json'
